f83a45afcc4da96a88b1a75463089901bb3af529
romankoll <114600254+romankoll@users.noreply.github.com>
2023-03-30 17:40:05 +0000

    Deploying to gh-pages from @ romankoll/goit-js-hw-09@39df9cb605bc29563a1424a43abad79b16c962fd 🚀

---
+++ 01-color-switcher.3b4fa032.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let e=null;t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.3b4fa032.js.map
